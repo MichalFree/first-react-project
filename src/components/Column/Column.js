096183358e270a7f4093e11c687ad626dac95a13
Michał Szymański <michal.szymanski1908@gmail.com>
2021-11-15 17:42:00 +0100

    Adding button and creator

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,12 +4,12 @@ import styles from './Column.scss';
 
 class Column extends React.Component {
   static propTypes = {
-    columnTitle: PropTypes.node.isRequired,
+    title: PropTypes.node.isRequired,
   }
   render() {
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}>{this.props.columnTitle}</h3>
+        <h3 className={styles.title}>{this.props.title}</h3>
       </section>
     )
   }
